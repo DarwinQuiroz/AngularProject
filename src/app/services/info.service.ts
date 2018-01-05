d961c7d56ba20a3466d9af5fa77f8dd0bb10bc21
DarwinQuiroz <darwin_quiroz04@hotmail.com>
2018-01-05 11:42:04 -0500

    Utilizando firebase

--- src/app/services/info.service.ts
+++ src/app/services/info.service.ts
@@ -5,16 +5,35 @@ import { Http } from '@angular/http';
 export class InfoService
 {
   info: any = {};
+  equipo: any = [];
   infoLista: boolean = false;
+  infoNosotrosLista: boolean = false;
 
   constructor(private http: Http)
   {
+    this.cargar_info();
+    this.cargar_nosotros();
+  }
+
+  cargar_info()
+  {
     this.http.get('assets/data/info.pagina.json').subscribe(
       data => {
+        console.log(data.json());
         this.info = data.json()
         this.infoLista = true;
       }
     )
   }
 
+  cargar_nosotros()
+  {
+    this.http.get('https://portafolio-83ae5.firebaseio.com/equipo.json').subscribe(
+      data => {
+        console.log(data.json());
+        this.equipo = data.json();
+        this.infoNosotrosLista = true;
+      }
+    )
+  }
 }
